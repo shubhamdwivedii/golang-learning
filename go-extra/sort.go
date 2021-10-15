c6cbdb8d4b029e6d9e189c34362376c0859b6168
Shubham Dwivedi <shubham00d@gmail.com>
2021-10-15 15:07:13 +0530

    sorting slice

--- go-extra/sort.go
+++ go-extra/sort.go
@@ -17,6 +17,16 @@ func main() {
 	s := sort.IntsAreSorted(ints) // in-built function to check if sorted.
 	fmt.Println("Sorted:", s)     // Sorted: true
 
+	// ###### Sort A Slice #############
+
+	colors := []string{"blue", "yellow", "red", "green"}
+
+	// This is similar to just implementing the Less() function
+	sort.Slice(colors, func(i, j int) bool {
+		return len(colors[i]) < len(colors[j])
+	})
+	fmt.Println(colors) // [red blue green yellow]
+
 	//###### Custom Sorting Functions ######
 
 	// Implement sort.Interface { Len(), Swap(), Less() }
@@ -28,14 +38,17 @@ func main() {
 
 type byLength []string
 
+// Len returns number of elements in the collection.
 func (s byLength) Len() int {
 	return len(s)
 }
 
+// How to swap the elements
 func (s byLength) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
+// How to decide if one item is less than other
 func (s byLength) Less(i, j int) bool {
 	return len(s[i]) < len(s[j])
 }
